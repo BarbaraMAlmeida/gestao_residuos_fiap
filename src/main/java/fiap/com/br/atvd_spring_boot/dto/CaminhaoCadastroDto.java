00a9b03fe0d2b74da9acd03c156fb5be324541a6
BarbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2024-11-04 21:46:26 -0300

    fix(validaçoes): ajuste validação caminhao placa

--- src/main/java/fiap/com/br/atvd_spring_boot/dto/CaminhaoCadastroDto.java
+++ src/main/java/fiap/com/br/atvd_spring_boot/dto/CaminhaoCadastroDto.java
@@ -2,12 +2,14 @@ package fiap.com.br.atvd_spring_boot.dto;
 
 import fiap.com.br.atvd_spring_boot.model.Caminhao;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CaminhaoCadastroDto(
         Long idCaminhao,
         @NotBlank(message = "O valor da placa é obrigatório")
+        @Max(value = 7, message = "A placa deve conter no máximo 7 caracteres")
         String placa,
         @NotNull(message = "O valor da capacidade é obrigatório")
         Long capacidade
